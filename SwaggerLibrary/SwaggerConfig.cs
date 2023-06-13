494ea7e3bc655e1369d1aaa7ec36eb6f5cfb509c
megha24 <agarwal.megha7@gmail.com>
2023-06-13 21:16:56 +0530

    created sagger library

---
+++ SwaggerLibrary/SwaggerConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace SwaggerLibrary;
+public static class SwaggerConfig
+{
+    public static void AddSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "Swagger Library API",
+                Description = "A sample API documentation using Swagger",
+                Contact = new OpenApiContact
+                {
+                    Name = "Your Name",
+                    Email = "your@email.com",
+                    Url = new Uri("https://your-website.com")
+                }
+            });
+        });
+    }
+}
